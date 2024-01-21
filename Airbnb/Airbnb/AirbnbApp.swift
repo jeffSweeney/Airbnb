faef64c36f2df20015ad50959e8e6293aa02b6d5
Jeff Sweeney <Jeffrey.Sweeney812.GH@gmail.com>
2024-01-21 13:48:24 -0500

    Initial app structure

---
+++ Airbnb/Airbnb/AirbnbApp.swift
@@ -0,0 +1,17 @@
+//
+//  AirbnbApp.swift
+//  Airbnb
+//
+//  Created by Jeffrey Sweeney on 1/21/24.
+//
+
+import SwiftUI
+
+@main
+struct AirbnbApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
